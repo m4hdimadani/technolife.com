a719782c52e4a587411d048ed32be1858c799a83
MAHDIMADANI <m4hdim4d4ni@gmail.com>
2025-04-24 18:28:26 +0330

    updating for offer

--- src/components/molcules/ProductsOff.js
+++ src/components/molcules/ProductsOff.js
@@ -12,7 +12,7 @@ import CountDown from "../../../utils/CountDown";
 
 function ProductsOff() {
   return (
-    <div className="w-[1600px] m-auto mt-10 border-2 border-[#ffd7d9] rounded-2xl">
+    <div className="w-[1600px] h-[481px] m-auto mt-10 border-2 border-[#ffd7d9] rounded-2xl">
       <div>
         <div className="flex justify-between bg-[#a2191f] text-white h-[53px] items-center p-8 rounded-2xl m-3">
           <h1 className="pr-2 text-white grow text-sm lg:text-2xl font-semiBold text-right">
@@ -23,23 +23,43 @@ function ProductsOff() {
             <IoIosArrowBack />
           </span>
         </div>
-        <div className="bor">
+        <div className="">
           <Swiper slidesPerView={6} navigation={true} modules={[Navigation]}>
             {imagesOff.map((img, index) => (
               <SwiperSlide key={index}>
-                <div>
-                  <span>{img.countDown}</span>
+                <div className=" border-b-2 border-[#da1e28] w-[186px] pb-[2px] m-auto mt-8">
+                  <div className="flex justify-around text-[#da1e28]  gap-12">
+                    <p className="text-sm font-bold">{img.letter}</p>
+                    <span className="font-bold">{img.countDown}</span>
+                  </div>
                 </div>
                 <div className="border-l-2 border-[#d8d3e4] ">
                   <div>
-                    <Image src={img.src} alt={img.alt} className="" />
-                    <p className="w-[186px] h-[50px]">{img.title}</p>
-                  </div>
-                  <div>
-                    <p className="text-base font-semiBold text-primary-shade-1 2md:text-xl">
-                      {img.price}
+                    <Image
+                      src={img.src}
+                      alt={img.alt}
+                      className="w-[186px] h-[186px] m-auto"
+                    />
+                    <p className="w-[186px] h-[50px] m-auto mt-5 truncate text-sm">
+                      {img.title}
                     </p>
-                    <span>{img.discount}</span>
+                  </div>
+                  <div className="h-[46px] w-full">
+                    <div className=" flex w-full items-center justify-around">
+                     <div className="min-w-6 lg:min-w-7 flex h-[17px] !items-center justify-center gap-1 rounded-[3px] px-1 lg:h-[19px] bg-red-600">
+                     <span className="flex !items-center justify-center pt-[1px] text-center font-semiBold leading-4 text-white lg:pt-[3px] !text-xs 2md:!text-sm">{img.discount}</span>
+                     </div>
+                     <div className="text-base font-bold text-primary-shade-1 2md:text-xl">
+                     <p >
+                        {img.price}
+                      </p>
+                     </div>
+                    </div>
+                    <div className=" flex  justify-end ">
+                      <p className="pt-0.5 ml-8 text-sm font-medium text-primary-tint-5 line-through ">
+                        {img.offerPrice}
+                      </p>
+                    </div>
                   </div>
                 </div>
               </SwiperSlide>
--- utils/CountDown.js
+++ utils/CountDown.js
@@ -13,16 +13,20 @@ function CountDown({ props }) {
     let total_seconds = parseInt(Math.floor(millisecond / 1000));
     let total_minutes = parseInt(Math.floor(total_seconds / 60));
     let total_hours = parseInt(Math.floor(total_minutes / 60));
-    
 
     let second = parseInt(total_seconds % 60);
     let minutes = parseInt(total_minutes % 60);
     let hours = parseInt(total_hours % 24);
 
-    return (`${hours}:${minutes}:${second}`)
+    return `${hours}:${minutes}:${second}`;
   };
 
-  return <div>{getFormattedTime(time)}</div>;
+  return (
+    <div>
+      <div className="text-red-700">{getFormattedTime(time)}</div>
+      <div></div>
+    </div>
+  );
 }
 
 export default CountDown;
