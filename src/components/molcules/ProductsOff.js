6d1667e3730e9181c5ade85e98ce81eaac2646d0
MAHDIMADANI <m4hdim4d4ni@gmail.com>
2025-04-24 10:27:36 +0330

    create countdown for project

--- src/components/molcules/ProductsOff.js
+++ src/components/molcules/ProductsOff.js
@@ -8,26 +8,39 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { IoIosArrowBack } from "react-icons/io";
 import Image from "next/image";
 import { imagesOff } from "../../../lib/imagesOff";
+import CountDown from "../../../utils/CountDown";
 
 function ProductsOff() {
   return (
     <div className="w-[1600px] m-auto mt-10 border-2 border-[#ffd7d9] rounded-2xl">
       <div>
         <div className="flex justify-between bg-[#a2191f] text-white h-[53px] items-center p-8 rounded-2xl m-3">
-            <h1 className="pr-2 text-white grow text-sm lg:text-2xl font-semiBold text-right">تکنوآف</h1>
-            <span className="flex items-center cursor-pointer gap-3 font-bold">
-                نمایش همه
-                <IoIosArrowBack />
-            </span>
+          <h1 className="pr-2 text-white grow text-sm lg:text-2xl font-semiBold text-right">
+            تکنوآف
+          </h1>
+          <span className="flex items-center cursor-pointer gap-3 font-bold">
+            نمایش همه
+            <IoIosArrowBack />
+          </span>
         </div>
-        <div className="">
+        <div className="bor">
           <Swiper slidesPerView={6} navigation={true} modules={[Navigation]}>
             {imagesOff.map((img, index) => (
               <SwiperSlide key={index}>
-                <div className="">
-                  <Image src={img.src} alt={img.alt} className="" />
-
-                  <p>{img.title}</p>
+                <div>
+                  <span>{img.countDown}</span>
+                </div>
+                <div className="border-l-2 border-[#d8d3e4] ">
+                  <div>
+                    <Image src={img.src} alt={img.alt} className="" />
+                    <p className="w-[186px] h-[50px]">{img.title}</p>
+                  </div>
+                  <div>
+                    <p className="text-base font-semiBold text-primary-shade-1 2md:text-xl">
+                      {img.price}
+                    </p>
+                    <span>{img.discount}</span>
+                  </div>
                 </div>
               </SwiperSlide>
             ))}
---
+++ utils/CountDown.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+function CountDown({ props }) {
+  const [time, setTime] = useState(props);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setTime(time - 1000);
+    }, 1000);
+  }, [time]);
+
+  const getFormattedTime = (millisecond) => {
+    let total_seconds = parseInt(Math.floor(millisecond / 1000));
+    let total_minutes = parseInt(Math.floor(total_seconds / 60));
+    let total_hours = parseInt(Math.floor(total_minutes / 60));
+    
+
+    let second = parseInt(total_seconds % 60);
+    let minutes = parseInt(total_minutes % 60);
+    let hours = parseInt(total_hours % 24);
+
+    return (`${hours}:${minutes}:${second}`)
+  };
+
+  return <div>{getFormattedTime(time)}</div>;
+}
+
+export default CountDown;
